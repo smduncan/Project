447c40f3aa0d27ddf2f41230c0fb231133fc68d1
Stuart Duncan <others@setynet.net>
2015-02-08 10:45:21 +0800

    Updated GyroStraightLine.c
    I can’t download this to my robot with a mac so it is untested

--- RobotC Sources/GyroStraightLine.c
+++ RobotC Sources/GyroStraightLine.c
@@ -1,14 +1,41 @@
+/*****************************
+*  GyroStraightLine.c
+*  08/02/2015 - Stuart Duncan
+*  Use the gyro to keep the
+*  robot traveling in a straight
+*  line
+*****************************/
+
+
+//Set up ports
 #define LEFT_WHEEL motorA
 #define RIGHT_WHEEL motorD
 #define GYRO S2
 
+// RobotC doesn't have a move steering function
+// but we can just make one.
+// This is similar to a "My Block"
+
 void moveSteering (int dir, int pwr) {
 	setMotorSpeed (LEFT_WHEEL, pwr/dir);
 	setMotorSpeed (RIGHT_WHEEL, pwr);
+
 }
 
-task main() {
-	moveSteering (0, 75);
-  delay (500);
+ // Tasks are like start blocks
+ // We can have many of these
+ // We must have a main task
 
+task main() {
+	resetGyro (GYRO);
+	while (true) {  //Starts in infinite loop
+		int gyro = getGyroDegrees (GYRO);
+		moveSteering (gyro, 75);
+
+		while (getGyroDegrees (GYRO) < 10 && getGyroDegrees (GYRO) > -10) {
+			// This empty loop acts as a wait block
+		}
+
+
+	}
 }
